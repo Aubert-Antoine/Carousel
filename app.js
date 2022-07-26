70c675183db8bf3eb55faf092ff1f5b0e60459dd
Antoine Aubert <antoine0aubert@edu.esiee.fr>
2022-07-26 17:55:40 +0200

    Final version

---
+++ app.js
@@ -0,0 +1,48 @@
+let images = [...document.querySelectorAll('.img')];
+let slider = document.querySelector('.slider')
+let sliderWidth;
+let imageWidth;
+let current = 0;
+let target = 0;
+let ease = .05;
+
+window, addEventListener('resize', init)
+
+images.forEach((img, idx) => {
+    img.style.backgroundImage = `url(./images/${idx + 1}.jpeg)`;
+})
+
+function lerp(start, end, t){
+    return start * (1-t) + end * t
+}
+
+function setTranform(el, transform){
+    el.style.transform = transform
+}
+
+function init(){
+    sliderWidth = slider.getBoundingClientRect().width;
+    imageWidth = sliderWidth / images.length;
+    document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`
+}
+
+function animate(){
+    current = parseFloat(lerp(current, target, ease)).toFixed(2);
+    target = window.scrollY;
+    setTranform(slider, `translateX(-${current}px)`)
+    animateImages();
+    requestAnimationFrame(animate);
+}
+
+function animateImages(){
+    let ratio = current / imageWidth;
+    let intersectionRation;
+
+    images.forEach((image, idx) => {
+        intersectionRationValue = ratio - (idx * 0.7);
+        setTranform(image, `translateX(${intersectionRationValue * 70}px)`)
+    })
+}
+
+init();
+animate()
--- v1/README.md
+++
@@ -1,3 +0,0 @@
-# Carousel
-Carousel is a one page web site wich expose picture.
-https://www.youtube.com/watch?v=4UXJb4rjeDg
